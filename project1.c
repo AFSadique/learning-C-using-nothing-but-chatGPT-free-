a6152b70db0b6a253c1f2620614489c0f258586e
AHMAD FARHAN SADIQUE <af.sadique.2007@gmail.com>
2026-07-20 00:50:51 +0600

    Update project1.c

--- project1.c
+++ project1.c
@@ -2,51 +2,7 @@
 
 int main()
 {
-    printf("i m leaning C,,,\n");
+    printf("i m leaning C by just chating with Chat GPT\n");
 
     return 0;
 }
-
-
-/*
-gcc -g project1.c -o app.exe
-.\app.exe
-
-gcc -g project2.c -o app.exe
-.\app.exe
-
-gcc -g project3.c -o app.exe
-.\app.exe
-
-gcc -g project4.c -o app.exe
-.\app.exe
-
-gcc -g project5.c -o app.exe
-.\app.exe
-
-gcc -g project6.c -o app.exe
-.\app.exe
-
-gcc -g project7.c -o app.exe
-.\app.exe
-
-gcc -g project8.c -o app.exe
-.\app.exe
-
-gcc -g project9.c -o app.exe
-.\app.exe
-
-gcc -g project10.c -o app.exe
-.\app.exe
-
-gcc -g project11.c -o app.exe
-.\app.exe
-
-gcc -g project12.c -o app.exe
-.\app.exe
-
-
-gcc -g 11-12-buffer.c -o app.exe
-.\app.exe
-
- */
